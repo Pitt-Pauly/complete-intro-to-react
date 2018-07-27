86bdf8219b30b318a8a51ffbda1a91074696e923
Pitt Pauly <pitt.pauly@gmail.com>
2018-07-27 17:47:15 +0100

    added snapshot testing with jest

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import preload from '../data.json';
 import ShowCard from './ShowCard';
 
-const Search = () => (
-  <div className="search">
-    {preload.shows.map(show => <ShowCard show={show} />)}
-  </div>
-);
+class Search extends Component {
+  state = {
+    searchTerm: ''
+  };
+
+  handleSearchTermChange = event => {
+    this.setState({ searchTerm: event.target.value });
+  };
+
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>h4p1 video</h1>
+          <input
+            onChange={this.handleSearchTermChange}
+            value={this.state.searchTerm}
+            type="text"
+            placeholder="Search"
+          />
+        </header>
+        <div>
+          {preload.shows
+            .filter(
+              show =>
+                `${show.title} ${show.description}`
+                  .toUpperCase()
+                  .indexOf(this.state.searchTerm.toUpperCase()) >= 0
+            )
+            .map(show => <ShowCard {...show} key={show.imdbID} />)}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Search;
