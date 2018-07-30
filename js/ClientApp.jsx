393c146b543bcf68fd39415f79126105e45972d5
Pitt Pauly <pitt.pauly@gmail.com>
2018-07-30 12:16:36 +0100

    added hot module reloading

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,21 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Landing from './Landing';
-import Search from './Search';
 
-const FourOhFour = () => <h1>404</h1>;
+import App from './App';
 
-const App = () => (
-  <BrowserRouter>
-    <div className="app">
-      <Switch>
-        <Route exact path="/" component={Landing} />
-        <Route exact path="/search" component={Search} />
-        <Route component={FourOhFour} />
-      </Switch>
-    </div>
-  </BrowserRouter>
-);
+const renderApp = () => {
+  render(<App />, document.getElementById('app'));
+};
+renderApp();
 
-render(<App />, document.getElementById('app'));
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    renderApp();
+  });
+}
